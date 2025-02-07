4a058e053fb14f843ac76017714761724d98074e
TimHarseth <tim@harseth.com>
2025-02-07 14:21:49 +0100

    jobbing

--- contracts/lab01_2.sol
+++ contracts/lab01_2.sol
@@ -2,5 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract rockPaperScissor {
-    function play (keccak256(abi.encodePacked(vote, salt)))
+    function play (keccak256(abi.encodePacked(string vote, bytes32 salt))) public {
+        return 0;
+    }
 }
