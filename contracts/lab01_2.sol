0a6c95f185e8680a0c79d92ca1e2bbfcf94616dd
TimHarseth <tim@harseth.com>
2025-02-12 12:00:52 +0100

    working on lab1_2

--- contracts/lab01_2.sol
+++ contracts/lab01_2.sol
@@ -1,8 +1,40 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract rockPaperScissor {
-    function play (keccak256(abi.encodePacked(string vote, bytes32 salt))) public {
-        return 0;
+contract RockPaperScissor {
+    uint public firstBet = 0;
+    
+    enum Moves {None, Rock, Paper, Scissor} // All possible moves
+    enum Won {PlayerA, PlayerB, Draw} // All outcomes
+
+    struct Game {
+        address playerA;
+        address playerB;
+        bytes32 encMoveA;
+        bytes32 encMoveB;
+        uint stake;
+    }
+
+    mapping(uint256 => Game) public games;
+    uint public gameID = 0;
+
+    function play(bytes32 hashedMove) public payable {
+        if (games[gameID].playerA == address(0)){ // first player
+            games[gameID] = Game({
+                playerA: msg.sender,
+                playerB: address(0),
+                encMoveA: hashedMove,
+                encMoveB: 0,
+                stake: msg.value
+            });
+        }
+        else { // second player
+            games[gameID] = Game({
+                Game storage game = games[gameIdCounter];
+
+                require(game.playerB == address(0), "Game is full");
+                require(msg.value == game.stake, "Stake must match");
+            })
+        }
     }
 }
